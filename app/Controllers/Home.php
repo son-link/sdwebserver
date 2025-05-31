77e071e8564d9241ac517d7609447541d241db7d
JamesAshwood <jamesashwood.contact@gmail.com>
2025-05-31 14:46:24 +0200

    Fixing identation

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,7 +4,7 @@ namespace App\Controllers;
 use App\Models\CarCatsModel;
 use App\Models\ChampionshipsBestLapsModel;
 use CodeIgniter\API\ResponseTrait;
-use App\Models\Lap_model; // 👈 Add this if not already present
+use App\Models\Lap_model;
 
 class Home extends BaseController
 {
@@ -140,8 +140,8 @@ class Home extends BaseController
 			$query = $this->db->query('SELECT *, DATE_FORMAT(date_start, "%d/%m/%Y") as date_start_conv, DATE_FORMAT(date_end, "%d/%m/%Y") AS date_end_conv FROM championship WHERE id < ? ORDER BY date_end DESC', [$data->id]);
 			if ($query && $query->getNumRows() > 0) $championships['previous'] = $query->getResult();
 
-      $lapModel = new Lap_model(); // Instantiate the model
-      $championships['current_races'] = $lapModel->get_fastest_lap_windows();
+			$lapModel = new Lap_model(); // Instantiate the model
+			$championships['current_races'] = $lapModel->get_fastest_lap_windows();
 
 			echo get_header('Championships', ['minidt.css']);
 			echo view('championships', $championships);

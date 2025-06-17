e2a00cc51149798fccd976d57b137059fe5ee663
Alfonso Saavedra "Son Link <sonlink.dourden@gmail.com>
2025-06-17 18:48:41 +0200

    RaceConfigModel: Fix issue if the query fail or not return data

--- app/Models/RaceConfigModel.php
+++ app/Models/RaceConfigModel.php
@@ -6,12 +6,12 @@ use CodeIgniter\Model;
 
 class RaceConfigModel extends Model
 {
-    protected $DBGroup = 'default';
+	protected $DBGroup = 'default';
 
-    public function getCurrentRaceConfig(): array
-    {
-        // Assumes there is only one active config, or the latest one is used
-		    $sql = '
+	public function getCurrentRaceConfig(): array
+	{
+		// Assumes there is only one active config, or the latest one is used
+		$sql = '
           SELECT c.*, tc.name AS category
           FROM championship c
           LEFT JOIN tracks_cats tc
@@ -21,37 +21,36 @@ class RaceConfigModel extends Model
           LIMIT 1;
         ';
 
-        $row = $this->db->query($sql)->getRowArray();
+		$query = $this->db->query($sql);
 
-        $wetnessValue = [
-          0 => "none",
-          1 => "light",
-          2 => "medium",
-          3 => "heavy",
-        ];
+		$wetnessValue = [
+			0 => "none",
+			1 => "light",
+			2 => "medium",
+			3 => "heavy",
+		];
 
-        if (count($row) > 0) {
-            return [
-              'track'     => $row["track_id"],
-              'category'  => $row["category"],
-              'laps'      => $row["raceLaps"],
-              'rain'      => $wetnessValue[$row["wettness"]],
-              'clouds'    => $row["clouds"],
-              'season'    => $row["season"],
-              'timeOfDay' => $row["timeOfDay"],
-          ];
-        }
-        
-        else {
-          return [
-              'track'     => 'ardennen-spa',
-              'category'  => 'circuit',
-              'laps'      => 10,
-              'rain'      => 'none',
-              'clouds'    => 'clear',
-              'season'    => 'summer',
-              'timeOfDay' => 'afternoon',
-          ];
-        }
-    }
+		if ($query && $query->getNumRows() > 0) {
+			$row = $query->getRowArray();
+			return [
+				'track'     => $row["track_id"],
+				'category'  => $row["category"],
+				'laps'      => $row["raceLaps"],
+				'rain'      => $wetnessValue[$row["wettness"]],
+				'clouds'    => $row["clouds"],
+				'season'    => $row["season"],
+				'timeOfDay' => $row["timeOfDay"],
+			];
+		} else {
+			return [
+				'track'     => 'ardennen-spa',
+				'category'  => 'circuit',
+				'laps'      => 10,
+				'rain'      => 'none',
+				'clouds'    => 'clear',
+				'season'    => 'summer',
+				'timeOfDay' => 'afternoon',
+			];
+		}
+	}
 }
